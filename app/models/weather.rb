0d812d37f8dd098d62d3a853fc93a5e1f95d54a7
Daniel Osorio <danielosorio83@gmail.com>
2017-09-12 14:45:07 -0500

    added weather search functionality

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -1,7 +1,34 @@
+require 'open_weather'
+
 class Weather
+  APPID = Rails.application.secrets.open_weather_app_id
+
+  DEFAULT_OPTIONS = { APPID: APPID, units: 'imperial' }
+
   class << self
     def search(city)
-      nil
+      response = by_city(city)
+      parse_response(response)
+    end
+
+    private
+
+    def by_city(city)
+      OpenWeather::Current.city(city, DEFAULT_OPTIONS)
+    rescue
+      { 'message' => 'Nothing was found! Please try again with a valid city and country.' }
+    end
+
+    def parse_response(response)
+      if response['main'].present?
+        { alert: 'success', message: "#{formatted_city_name(response)}: #{response['main']['temp']} ºF" }
+      else
+        { alert: 'danger', message: response['message'] }
+      end
+    end
+
+    def formatted_city_name(response)
+      "#{response['name']}, #{response['sys']['country']}"
     end
   end
 end
--- spec/models/weather_spec.rb
+++ spec/models/weather_spec.rb
@@ -5,10 +5,108 @@ RSpec.describe Weather, type: :model do
   describe 'Public Class Method' do
     describe '.search' do
       let(:address) { '' }
-      
-      it 'returns nil' do
+
+      it 'calls `by_city` with `address`' do
+        expect(Weather).to receive(:by_city).with(address).and_call_original
+        Weather.search(address)
+      end
+
+      it 'calls `parse_response`' do
+        expect(Weather).to receive(:parse_response).with(kind_of(Hash))
+        Weather.search('')
+      end
+
+      it 'returns a valid hash' do
         weather = Weather.search(address)
-        expect(weather).to be_nil
+        expect(weather).to be_a(Hash)
+        expect(weather).to have_key(:alert)
+        expect(weather).to have_key(:message)
+      end
+    end
+  end
+
+
+  describe 'Private Class Method' do
+    describe '.by_city(city)' do
+      let(:city){ 'Barranquilla' }
+
+      context 'when error raised' do
+        it 'returns the custom error message' do
+          allow(OpenWeather::Current).to receive(:city).and_raise('Error')
+          response = { 'message' => 'Nothing was found! Please try again with a valid city and country.' }
+          expect(Weather.send(:by_city, city)).to eq(response)
+        end
+      end
+      
+      it 'calls a `OpenWeather::Current.city` with valid arguments' do
+        expect(OpenWeather::Current).to receive(:city).with(city, Weather::DEFAULT_OPTIONS)
+        Weather.send(:by_city, city)
+      end
+
+      context 'when `city is valid' do
+        it 'returns a valid hash' do
+          weather = Weather.send(:by_city, city)
+          expect(weather).to be_a(Hash)
+          expect(weather).to have_key('main')
+        end
+      end
+
+      context 'when `city` is invalid' do
+        it 'returns a valid hash' do
+          weather = Weather.send(:by_city, '')
+          expect(weather).to be_a(Hash)
+          expect(weather).to have_key('message')
+        end
+      end
+    end
+
+    describe '.parse_response(response)' do
+      context 'when `response` includes `main`' do
+        let(:response) { { 'main' => { 'temp' => '70' } } }
+        let(:parse_response) { Weather.send(:parse_response, response) }
+
+        it 'returns :alert with `success`' do
+          allow(Weather).to receive(:formatted_city_name).with(response)
+          expect(parse_response).to have_key(:alert)
+          expect(parse_response[:alert]).to eq('success')
+        end
+
+        it 'returns :message to include `ºF`' do
+          allow(Weather).to receive(:formatted_city_name).with(response)
+          expect(parse_response).to have_key(:message)
+          expect(parse_response[:message]).to include(response['main']['temp'])
+          expect(parse_response[:message]).to include('ºF')
+        end
+
+        it 'returns :message to include the temp' do
+          allow(Weather).to receive(:formatted_city_name).with(response)
+          expect(parse_response).to have_key(:message)
+          expect(parse_response[:message]).to include(response['main']['temp'])
+        end
+      end
+
+      context 'when `response` NOT includes `main`' do
+        let(:response) { { 'message' => 'city not found' } }
+        let(:parse_response) { Weather.send(:parse_response, response) }
+
+        it 'returns :alert with `danger`' do
+          expect(parse_response).to have_key(:alert)
+          expect(parse_response[:alert]).to eq('danger')
+        end
+
+        it 'returns :message with `city not found`' do
+          expect(parse_response).to have_key(:message)
+          expect(parse_response[:message]).to eq(response['message'])
+        end
+      end
+    end
+
+    describe '.formatted_city_name(response)' do
+      let(:response) { { 'sys' => { 'country' => 'CO' }, 'name' => 'Barranquilla' } }
+
+      it 'returns the expected string' do
+        formatted_city_name = "#{response['name']}, #{response['sys']['country']}"
+        expect(Weather.send(:formatted_city_name, response)).to eq(formatted_city_name)
       end
     end
   end
