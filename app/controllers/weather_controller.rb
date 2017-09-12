0c0a1f7285729356ca7a0302f18b3d0087f68fdb
Daniel Osorio <danielosorio83@gmail.com>
2017-09-12 13:46:48 -0500

    /weather now responds as js

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,5 +1,9 @@
 class WeatherController < ApplicationController
   # GET /weather
   def index
+    respond_to do |format|
+      format.js
+      format.html { redirect_to root_url }
+    end
   end
 end
--- spec/controllers/weather_controller_spec.rb
+++ spec/controllers/weather_controller_spec.rb
@@ -1,10 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe WeatherController, type: :controller do
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+  describe 'GET #index' do
+    context 'when format is `html`' do
+      it 'redirects to root_url' do
+        get :index
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(root_url)
+      end
+    end
+
+    context 'when format is `js`' do
+      it 'returns http success' do
+        get :index, xhr: true
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 end
