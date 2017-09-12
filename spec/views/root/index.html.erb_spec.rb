5dbbd6cfafb09f321269022f9fed35d27bb3c49a
Daniel Osorio <danielosorio83@gmail.com>
2017-09-12 13:38:29 -0500

    added form input/button into root

--- spec/views/root/index.html.erb_spec.rb
+++ spec/views/root/index.html.erb_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "root/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'contains a `form`' do
+    render template: "root/index.html.erb"
+    expect(rendered).to match /form/
+  end
+
+  it 'contains a `weather` id' do
+    render template: "root/index.html.erb"
+    expect(rendered).to match /weather/
+  end
+
+  it 'display an input for the `address`' do
+    render template: "root/index.html.erb"
+    expect(rendered).to match /address/
+  end
+
+  it 'display a button for the `get-weather`' do
+    render template: "root/index.html.erb"
+    expect(rendered).to match /get-weather/
+  end
 end
